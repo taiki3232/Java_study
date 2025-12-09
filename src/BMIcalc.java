e5a3409ea5053fc903dad4b1ac3f9bbda6239216
taiki3232 <tai.hamada@icloud.com>
2025-12-09 11:20:01 +0900

    BMIcalculate loop

--- src/BMIcalc.java
+++ src/BMIcalc.java
@@ -2,21 +2,29 @@ import java.util.Scanner;
 
 public class BMIcalc {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("体重(kg)を入力してください");
-        int weight = sc.nextInt();
-        System.out.println("身長(m)を入力してください(例:1.7)");
-        double height = sc.nextDouble();
-        int showBMI = (int) Math.floor(weight / (height * height));
-        double BMI = (weight / (height * height));
-        System.out.println("あなたのBMIは" + showBMI + "です");
 
-        if (BMI < 18.5) {
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("体重(kg)を入力してください");
+            int weight = sc.nextInt();
+            System.out.println("身長(m)を入力してください(例:1.7)");
+            double height = sc.nextDouble();
+            int showBMI = (int) Math.floor(weight / (height * height));
+            double BMI = (weight / (height * height));
+            System.out.println("あなたのBMIは" + showBMI + "です");
+            if (BMI < 18.5) {
             System.out.println("痩せ型です");
-        } else if (BMI >= 25) { 
-            System.out.println("肥満です");
-        } else {
-            System.out.println("標準体型です");
+                } else if (BMI >= 25) { 
+                    System.out.println("肥満です");
+                    } else {
+                        System.out.println("標準体型です");
+                        }
+            System.out.println("もう一度計算しますか？ (y/n)");
+            String check = sc.next();
+            if (check.equals("n")) { 
+                System.out.println("終了します");
+                break;
+            }
         }
     }
 }
