9df124da320e41defb7d9909e4078574645bab7c
taiki3232 <tai.hamada@icloud.com>
2025-12-09 11:20:51 +0900

    BMIcalculate loop

--- src/BMIcalc.java
+++ src/BMIcalc.java
@@ -2,9 +2,8 @@ import java.util.Scanner;
 
 public class BMIcalc {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
         while (true) {
-            Scanner sc = new Scanner(System.in);
             System.out.println("体重(kg)を入力してください");
             int weight = sc.nextInt();
             System.out.println("身長(m)を入力してください(例:1.7)");
